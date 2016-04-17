c27fceb5aee71ae180d3ec3f94a03e305b8a1515
Taha Emara <tahaemara.eng@gmail.com>
2016-04-17 11:13:23 +0200

    Update mouse.ino

--- mouse.ino
+++ mouse.ino
@@ -14,16 +14,14 @@ char mx = 0;
 char my = 0;
 void setup()
 {
-  //Serial.begin(38400);
   mouse.initialize();
   mouse.set_resolution(02);
-  
   pinMode(f, OUTPUT);
   pinMode(b, OUTPUT);
   pinMode(l, OUTPUT);
   pinMode(r, OUTPUT);
-digitalWrite(f, 1);//ACTIVE LOW
-    digitalWrite(b, 1);
+  digitalWrite(f, 1);//ACTIVE LOW
+  digitalWrite(b, 1);
   digitalWrite(l, 1);
   digitalWrite(r, 1);
 }
@@ -46,42 +44,36 @@ void loop()
     left(mx);
 }
 void forward(int del){
-  // Serial.println("Begin");
-    delay(20);
-    digitalWrite(f, 0);
-    digitalWrite(b, 1);
+  delay(20);
+  digitalWrite(f, 0);
+  digitalWrite(b, 1);
   digitalWrite(l, 1);
   digitalWrite(r, 1); // delay(500);
-   delay(abs(del)*2);
-  
-  //  Serial.println("Einde");
+  delay(abs(del)*2);
 }
 
 void backward(int del){ delay(20);
-    digitalWrite(b, 0);
-   digitalWrite(l, 1);
+  digitalWrite(b, 0);
+  digitalWrite(l, 1);
   digitalWrite(r, 1);
-   digitalWrite(f, 1);
-   delay(abs(del)*2);
- // delay(500);
+  digitalWrite(f, 1);
+  delay(abs(del)*2);
 }
 
 void left(int del){ delay(20);
-     digitalWrite(l, 0);
-   
-     digitalWrite(b, 1);
+  digitalWrite(l, 0);
+  digitalWrite(b, 1);
   digitalWrite(r, 1);
-   digitalWrite(f, 1); // delay(500);
-   delay(abs(del)*2);
+  digitalWrite(f, 1); // delay(500);
+  delay(abs(del)*2);
 }
 
 void right(int del){ delay(20);
-      digitalWrite(r, 0);
-   
-     digitalWrite(l, 1);
+  digitalWrite(r, 0);
+  digitalWrite(l, 1);
   digitalWrite(b, 1);
-   digitalWrite(f, 1);  //delay(500);
-   delay(abs(del)*2);
+  digitalWrite(f, 1);  //delay(500);
+  delay(abs(del)*2);
 }
 
 
